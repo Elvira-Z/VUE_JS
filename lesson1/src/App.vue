b3b4e2a678d763ac494beb5810d3b169c92b22b1
Elvira_Z <elia15@list.ru>
2022-09-04 17:28:15 +0300

    lesson2

--- lesson1/src/App.vue
+++ lesson1/src/App.vue
@@ -2,19 +2,26 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <Calc />
+    <Calc v-if="show" />
+    <!-- <Calc v-if="show" :key="1" />
+    <Calc v-if="!show" :key="2" /> -->
+    <!-- <button @click="show = !show">click</button> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Calc from './components/Calc.vue'
+
 export default {
   name: 'App',
   components: {
     HelloWorld,
     Calc
-  }
+  },
+  data: () => ({
+    show: true
+  })
 }
 </script>
 
